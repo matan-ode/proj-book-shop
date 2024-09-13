a88cada373c5b68a93325ea1097058b6b59f111b
matan-ode <matanodentz@gmail.com>
2024-09-13 14:03:24 +0300

    Add dynamic rendering from model

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,7 +1,24 @@
 'use strict'
 
 
-function onInit(){
+function onInit() {
     render()
 }
 
+function render() {
+    const elBooks = document.querySelector('.books-table')
+    const books = getBooks()
+    var strHtml = books.map(book => 
+        `<tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>
+                <button class="read-btn" onclick="onReadBook(event, this)">Read</button>
+                <button class="update-btn" onclick="onUpdateBook(event, this)">Update</button>
+                <button class="delete-btn" onclick="onDeleteBook(event, this)">Delete</button>
+            </td>
+        </tr>`
+    )
+    
+    elBooks.innerHTML = strHtml.join('')
+}
